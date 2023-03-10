e52fad213e3dad0bfe1ea587a3f72f136e9d955c
Ryan Showers <ryanshowers@6gwebdesign.com>
2023-03-10 07:21:23 -0700

    use preview for entries

--- controllers/entries/preview.php
+++ controllers/entries/preview.php
@@ -1,17 +1,23 @@
 <?php Block::put('breadcrumb') ?>
-    <ul>
-        <li><a href="<?= Backend::url('sixgweb/forms/entries') ?>">Entries</a></li>
-        <li><?= e($this->pageTitle) ?></li>
-    </ul>
+<ul>
+    <li><a href="<?= Backend::url('sixgweb/forms/entries') ?>">Entries</a></li>
+    <li><?= e($this->pageTitle) ?></li>
+</ul>
 <?php Block::endPut() ?>
 
-<?php if (!$this->fatalError): ?>
+<?php if (!$this->fatalError) : ?>
+
+    <div class="control-toolbar" data-control=" toolbar">
+        <a href="<?= Backend::url('sixgweb/forms/entries/update/' . $formModel->id) ?>" class="btn btn-primary oc-icon-pencil">
+            Edit Entry
+        </a>
+    </div>
 
     <div class="form-preview">
         <?= $this->formRenderPreview() ?>
     </div>
 
-<?php else: ?>
+<?php else : ?>
 
     <p class="flash-message static error"><?= e($this->fatalError) ?></p>
     <p><a href="<?= Backend::url('sixgweb/forms/entries') ?>" class="btn btn-default"><?= e(trans('backend::lang.form.return_to_list')) ?></a></p>
